cc98390cfa3ce5268152ff0625626ce0bedde5d3
Eugen Albrecht <ea0@gmx.net>
2019-09-09 23:18:49 +0200

    add sqlite check and create db file

--- install.sh
+++ install.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+
+# check if sqlite is available
+if ! hash sqlite3 2>/dev/null
+then
+    echo "'sqlite3' was not found in PATH"
+    exit 1
+fi
+
+
+# Create sqlite DB
+FILE="/log.sqlite"
+if test -f "$FILE"; then
+    echo "$FILE exist"
+    exit 1
+else
+    echo "$FILE does not exist"
+    sqlite3 $FILE < sql/table_log
+fi
+
+echo "DONE!"
