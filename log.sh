af3fa9062ee1b0466f9d141daa9876e6bd795f48
Eugen Albrecht <ea0@gmx.net>
2019-09-09 23:23:39 +0200

    add log.sh

---
+++ log.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+shopt -s extglob
+
+current_user=$(whoami)
+
+#/root/log/winlist.sh | while IFS= read -r line ; do
+/root/log/currentwin | while IFS= read -r line ; do
+### Trim leading whitespaces ###
+line="${line##*( )}"
+
+### trim trailing whitespaces  ##
+line="${line%%*( )}"
+
+sqlite3 /log.sqlite "INSERT INTO log (window, user) VALUES ('$line', '$current_user')"
+done
+
