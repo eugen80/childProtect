1cfa084f20b43627501002c6140fd545b3b1dbb1
Eugen Albrecht <ea0@gmx.net>
2019-10-19 12:26:10 +0200

    delete subgroups

--- config.py
+++ config.py
@@ -8,7 +8,6 @@
 config = {}
 config['fun'] = {}
 config['fun']['maxMin'] = 90
-config['fun']['grp'] = 1
 config['fun']['apps'] = {}
 config['fun']['apps']['minecraft'] = 1
 config['fun']['apps']['youtube'] = 1
@@ -16,24 +15,20 @@ config['fun']['apps']['minetest'] = 1
 
 config['browser'] = {}
 config['browser']['maxMin'] = 90
-config['browser']['grp'] = 1
 config['browser']['apps'] = {}
 config['browser']['apps']['firefox'] = 1
 
 config['communication'] = {}
 config['communication']['maxMin'] = 60
-config['communication']['grp'] = 2
 config['communication']['apps'] = {}
 config['communication']['apps']['whatsup'] = 1
 
 config['productive'] = {}
 config['productive']['maxMin'] = 180
-config['productive']['grp'] = 3
 config['productive']['apps'] = {}
 config['productive']['apps']['schlaukopf'] = 1
 
 config['misc'] = {}
 config['misc']['maxMin'] = 180
-config['misc']['grp'] = 4
 config['misc']['apps'] = {}
 config['misc']['apps']['konsole'] = 1
